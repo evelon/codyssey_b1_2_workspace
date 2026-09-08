6921d56739c1c41633aff3245f7f37738d80b798
Jonghyun Lim <ezemango@gmail.com>
2026-09-08 20:33:51 +0900

    style: apply prettier default style

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
-import { useEffect } from 'react'
-import { supabase } from './lib/supabase'
+import { useEffect } from "react";
+import { supabase } from "./lib/supabase";
 
 function App() {
   useEffect(() => {
-    supabase.from('movie_reviews').select('*').then(console.log)
-  }, [])
+    supabase.from("movie_reviews").select("*").then(console.log);
+  }, []);
 
-  return (
-    <div>Hello</div>
-  )
+  return <div>Hello</div>;
 }
 
-export default App
+export default App;
