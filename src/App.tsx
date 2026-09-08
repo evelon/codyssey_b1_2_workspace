7131380910952756b2c2b6ae7de7eae18c4e8ad6
Jonghyun Lim <ezemango@gmail.com>
2026-09-08 20:25:49 +0900

    feat: ready to connect to supabase

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,11 @@
+import { useEffect } from 'react'
+import { supabase } from './lib/supabase'
+
 function App() {
+  useEffect(() => {
+    supabase.from('movie_reviews').select('*').then(console.log)
+  }, [])
+
   return (
     <div>Hello</div>
   )
